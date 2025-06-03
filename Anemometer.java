a289b5347928e1581d719d51f8ff474f95d24634
TLi-svg <timothyl86@nycstudents.net>
2025-06-03 11:48:03 -0400

    Update and rename finchcode.java to Anemometer.java
    - Added WIP measure distances method
    - Updated instance variables to be private
    - Created a finch object to be used later

---
+++ Anemometer.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Anemometer {
+  Finch bird = new Finch();
+  // use pause to create a set "pulse", allowing for a measured interval of windspeed as measured by the size of the turbine. 
+  private int rotations; 
+  private int seconds; 
+  private double speed; 
+
+  
+  public static void main(String[] args) {
+
+  }
+
+
+  // Measure distances to be used in later calculations for wind speed
+  public static ArrayList<int> measure(int len) {
+    ArrayList<int> distances = new ArrayList<>;
+    for (int i = 0; i < len; i++) {
+    }
+  }
+
+
+  
+}
